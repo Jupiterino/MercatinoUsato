11da982907450eac377996bf34741951162fb79e
giomosca <giova@gmail.com>
2024-05-22 21:56:24 +0200

    first commit

---
+++ RegistrazioneCode.php
@@ -0,0 +1,50 @@
+<?php
+
+    session_start();
+    include('script.php');
+
+    if( empty($_SESSION["loggato"]) ){
+                
+        header("Location: LoginPage.html");
+    }
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $nome = $_POST["nome"];
+    $cognome = $_POST["cognome"];
+    $eta = $_POST["eta"];
+
+    /*if() {
+
+        //If che controlla la mail, accettate solo gmail, virgilio, yahoo, libero
+
+    }*/
+    $HASHpassword = hash('sha256', $password);
+    $sql = "INSERT INTO utente (Nome, Cognome, Email, Password, Eta) VALUES ('$nome', '$cognome', '$email', '$HASHpassword', '$eta')";
+    if ($conn->query($sql) == TRUE) {
+
+        if($conn -> affected_rows == 0){
+
+            $_SESSION["messaggio"] = "Error creating User: " . $conn->error;
+            header('Location: Messaggio.php');
+        }
+        
+        else {
+
+            $_SESSION['nome'] = $nome;
+            $_SESSION['email'] = $email;
+            $_SESSION['pw'] = $HASHpassword;
+            $_SESSION["loggato"] = "log";
+            $_SESSION["messaggio"] = "New User created successfully";
+            header('Location: Messaggio.php');
+        }
+
+    }
+    
+    else {
+
+        $_SESSION["messaggio"] = "Error creating User: " . $conn->error;
+        header('Location: Messaggio.php');
+    }
+
+?> 
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+    session_start();
+    include('script.php');
+    //unset($_SESSION["loggato"]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Index</title>
+    </head>
+    <body>
+
+        <!--<a href="LoginPage.html">pagina Login</a>-->
+
+        <?php
+        if( !empty($_SESSION['loggato'])){
+            
+            echo "<h1>Benvenuto " . $_SESSION['nome'] . "</h1>";
+        }
+            
+
+            $sql = "SELECT oggetto.Nome, Foto, Descrizione, utente.Nome, utente.Email, utente.Password FROM oggetto JOIN utente ON oggetto.IdUtente = utente.ID";   
+            $result = $conn->query($sql);
+            $COLONNA = mysqli_fetch_assoc($result);
+
+            if ($result->num_rows > 0) {
+                while($COLONNA = $result->fetch_assoc()) {
+                if($_SESSION['pw'] != $COLONNA['Password'] && $_SESSION['email'] != $COLONNA['Email']){
+                
+                    echo "<div class='box'>";
+                    echo "<img src= '" . $COLONNA['Foto'] .  "' alt='foto' class='Fotoo'>";
+                    echo "<div>";
+                    echo "<h4><b> " . $COLONNA['Nome'] . "</b></h4>";
+                    echo "<p>" . $COLONNA['Descrizione'] . "</p>";
+                    echo "<p> caricata da " . $COLONNA['Nome'] . "</p>";
+                    echo "</div>";
+                    echo "</div>";
+                }   
+                
+                }
+
+            }
+            else {
+                echo "0 results";
+            }
+            
+           
+        ?><br>
+
+        <a class='btn btn-primary' href=' 
+        <?php 
+            if( !empty($_SESSION["loggato"]) ) {
+                ?>
+                https://www.youtube.com/
+                <?php
+            }
+            else{
+                ?>
+                LoginCode.php
+                <?php
+            }
+        ?>
+        ' role='button'>Link</a>
+        <br><br>
+        <a class='btn btn-primary' href='profilo.php' role='button'>PROFILO PERSONALE</a><br><br>
+        <a class='btn btn-primary' href='scriptlogout.php' role='button'>LOG OUT</a><br><br>
+    </body>
+
+</html>
